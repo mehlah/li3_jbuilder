5de87b6a170920559bfd9a41cc8dfbbfab4fbc56
Mehdi Lahmam B <mehdi@lahmam.com>
2013-04-12 11:21:25 +0200

    Overrides the default json media type, only if the response type is json, to allow rendering a json template

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -4,29 +4,27 @@ use lithium\action\Dispatcher;
 use lithium\net\http\Media;
 
 /**
- * DISCLAIMER: This is the dumbest idea ever, but get things done at the moment.
- * This filter overrides the default `json` registered media type, only if the request type is `json`.
- * This allows to build custom `json` responses by rendering a template,
- * and still use the default `json` type when rendering a response isn't necessary (`$coll->to('json')`)
+ * This filter overrides the default `json` media type, only if the response type is `json`.
+ * This allows to build custom `json` responses by rendering a template.
  */
-Dispatcher::applyFilter('_callable', function($self, $params, $chain) {
+Media::applyFilter('render', function($self, $params, $chain) {
 
-	if ($params['request']->type() == 'json') {
+	if ($params['response']->type() === 'json') {
 
-		Media::type('json', 'application/json', array(
+		$params['options'] += [
 			'view' => 'lithium\template\View',
-			'paths' => array(
-				'template' => '{:library}/views/{:controller}/{:template}.{:type}.php',
+			'paths' => [
 				'layout'   => false,
-				'element'  => '{:library}/views/elements/{:template}.{:type}.php'
-			),
-			'conditions' => array('type' => true)
-		));
-
+				'template' => '{:library}/views/{:controller}/{:template}.json.php',
+				'element'  => '{:library}/views/elements/{:template}.json.php'
+			],
+			'cast' => false,
+			'encode' => false,
+			'decode' => false
+		];
 	}
 
 	return $chain->next($self, $params, $chain);
-
 });
 
 ?>
